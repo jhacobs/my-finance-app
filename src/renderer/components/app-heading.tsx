61e11e59a8ef20be13eb2df6c76f9fed2f9217a8
Jacob Aretz <jacob.aretz@gmail.com>
2026-07-15 00:46:19 +0200

    Fix app title

--- src/renderer/components/app-heading.tsx
+++ src/renderer/components/app-heading.tsx
@@ -2,7 +2,7 @@ export default function AppHeading() {
   return (
     <header className="mb-5">
       <div className="flex items-center mb-2">
-        <h1 className="text-3xl font-bold tracking-tight">My finance app</h1>
+        <h1 className="text-3xl font-bold tracking-tight">My Finance App</h1>
       </div>
       <h2 className="text-muted-foreground">
         Track your income and expenses all in one place.
